7d04bdcf5658b41c23dee4ec33c93d2675e6e79b
cherrycookie27 <jussilatara@gmail.com>
2025-01-13 15:19:58 +0200

    wowowo

--- Witchcraft/Assets/Scripts/enemy.cs
+++ Witchcraft/Assets/Scripts/enemy.cs
@@ -7,16 +7,8 @@ public class enemy : MonoBehaviour
 
     public GameObject player;
     public float speed;
-    
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
